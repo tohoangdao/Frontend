6e99d5afa011a05a9d6822c5f69eccb74dd8af18
To Hoang Dao <63783327+tohoangdao@users.noreply.github.com>
2024-12-08 13:34:24 +0700

    update

--- src/components/main/HomeStoreCard.tsx
+++ src/components/main/HomeStoreCard.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from "next/link";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function HomeStoreCard(data: any) {
@@ -6,7 +5,7 @@ function HomeStoreCard(data: any) {
     <div className="flex flex-col">
       <Link href={`/store/${data._id}`} className="hover:underline">
         <div className="relative h-48">
-          <Image src={data.image} alt="store" layout="fill" objectFit="cover" />
+          <img src={data.image} alt="store" />
         </div>
 
         <h2 className="text-xl font-bold">{data.name}</h2>
